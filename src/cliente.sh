f40179956efb8b16ee22587720e891bea266f658
xulegaspi <xulegaspi@gmail.com>
2014-05-01 13:34:29 +0200

    commit

--- src/cliente.sh
+++ src/cliente.sh
@@ -2,7 +2,7 @@
 #
 # Recibe un argumento: el nombre del ordenador en el que se ha arrancado
 # el Servidor de Eco
-if ($#argv != 1) then
+if ($#argv < 1) then
 cat << end-of-help-message
    uso: cliente.sh máquina
 	  máquina    nombre de la máquina en la que corre el Servidor de Eco
@@ -10,4 +10,4 @@ end-of-help-message
 exit(-1)
 endif
 setenv CLASSPATH ../bin
-java -Djava.security.policy=policy.all Client $1 -k 0 $2
+java -Djava.security.policy=policy.all Client $1 -k 0 < $2
