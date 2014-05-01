1a9c06390bdbfffe2987c70a5f71f112accd3ea4
xulegaspi <xulegaspi@gmail.com>
2014-05-01 13:28:08 +0200

    commit

--- src/Server.java
+++ src/Server.java
@@ -11,7 +11,7 @@ public class Server {
         }
         else{
 		    try {
-                if(args.length>2) {
+                if(args.length>=2) {
                     if(args[0].equals("-k") || args[0].equals("-K")) {
                         Key = args[1];
                         System.out.println("Admin key: " + Key);
