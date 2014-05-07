c3654b5eaee12b8515b7133b96eafcb0968eaa53
lorgaco <lorenzo.gaco@gmail.com>
2014-05-07 17:03:35 +0200

    Structs done

--- src/Answer.java
+++ src/Answer.java
@@ -1,26 +1,27 @@
+import java.io.Serializable;
 import java.util.List;
 
-class AnswerNuevo{
+class AnswerNuevo implements Serializable {
 	int server_error;
     sNuevo answer;
 }
 
-class AnswerInt{
+class AnswerInt implements Serializable {
     int server_error;
     int answer;
 }
 
-class AnswerPlantilla{
+class AnswerPlantilla implements Serializable {
    int server_error;
     List<Jugador> answer;
 }
 
-class AnswerRepertorio{
+class AnswerRepertorio implements Serializable {
     int server_error;
     List<Juego> answer;
 }
 
-class AnswerLista{
+class AnswerLista implements Serializable {
     int server_error;
     sLista answer;
 }
